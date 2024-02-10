0ff7d612fac6f98753519d83dbb5afe8a08e01ee
Fabio Marques <fabio.marquezzx@gmail.com>
2024-02-10 10:13:23 -0300

    workflow

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "lambda" {
   filename      = "lambda.zip" # Nome do arquivo zip contendo seu código Node.js
   function_name = var.lambda_name
   role          = aws_iam_role.lambda_role.arn
-  handler       = "app/index.handler"
+  handler       = data.archive_file.zip.output_base64sha256
   runtime       = "nodejs14.x"
 
   environment {
@@ -12,4 +12,16 @@ resource "aws_lambda_function" "lambda" {
   }
 
   depends_on = [aws_iam_role.lambda_role]
+}
+
+
+data "archive_file" "zip" {
+  excludes = [
+    "infra/",
+    ".github/",
+    ".gitignore"
+  ]
+  type = "zip"
+  source_file = path.module
+  output_path = "${path.module}/lambda.zip"
 }
