433787cf18fe9dc75e06ec81aa436ead17e9bd31
Fabio Marques <fabio.marquezzx@gmail.com>
2024-02-09 21:50:17 -0300

    feat: creating unit test and organizing folders

---
+++ app/src/index.ts
@@ -0,0 +1,10 @@
+const handler = async (event) => {
+  console.log('Lambda function executed successfully!');
+  return {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+};
+
+
+export { handler }
---
+++ app/test/Index.test.ts
@@ -0,0 +1,13 @@
+import { handler } from "../src/index"
+
+
+describe("CreateClientService", () => {
+
+
+
+    it("Test", async () => {
+        let response = handler(null)
+        expect(response).toBeTruthy();
+
+    });
+});
