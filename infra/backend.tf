cdece38f1214793f85691058ff473323940dc287
Fabio Marques <46504380+Marques0x01@users.noreply.github.com>
2024-06-24 23:38:57 -0300

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "fiap-backend"
+    bucket         = "tfstates-fiap"
     key            = "lambda-auth/terraform.tfstate"
     region         = "us-east-1"
     
