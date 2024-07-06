1ab39f70149b7d88b7e115127e3f2c00dc23d123
Fabio Marques <46504380+Marques0x01@users.noreply.github.com>
2024-07-06 11:10:29 -0300

    Feature/change account (#18)
    * teste commit 4
    
    * chaning tfstate name
    
    * changing node version
    
    * feat: changing client id from cognito
    
    * changing client id
    
    * changing backend
    
    * fixing pipe
    
    * restart pipe
    
    * changing api key
    
    * fix: fixing backend
    
    * change backend
    
    * Change region
    
    * change account

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tfstates-fiap-lanches"
+    bucket         = "backend-fiap"
     key            = "lambda-auth/terraform.tfstate"
     region         = "us-east-2"
     
